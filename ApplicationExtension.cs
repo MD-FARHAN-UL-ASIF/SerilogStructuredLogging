acb21fbba80b48b48f0d25d594aa97b44d32f493
farhanx <farhanchowdhury66@gmail.com>
2025-04-06 16:27:06 +0600

    #Configure_Serilog

---
+++ ApplicationExtension.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace SerilogStructuredLogging
+{
+    public static class ApplicationExtension
+    {
+        public static void ConfigureSerilog(this IHostBuilder host)
+        {
+            host.UseSerilog((ctx, lc) =>
+            {
+                lc.WriteTo.Console();
+            });
+        }
+    }
+}
